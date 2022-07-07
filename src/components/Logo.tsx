5772277e4d512b0bd3eac2667e8e6c6e1ef03427
Fernando Medeiros <fernandohenrique520@live.com>
2022-07-07 17:58:09 -0300

    fix logo error

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 export function Logo() {
   return (
-    <strong className="py-4 ml-[90%]"><h1><a href="/event"><img className=" border border-green-900" src="https://yt3.ggpht.com/ytc/AKedOLS2A9Yrw17hQUyDUBwwtjZ5clMY80O1wSRqQjoi=s88-c-k-c0x00ffffff-no-rj" alt="logo" /></a></h1></strong>
+    <strong className="py-4 ml-[90%]"><h1><a href="/"><img className=" border border-green-900" src="https://yt3.ggpht.com/ytc/AKedOLS2A9Yrw17hQUyDUBwwtjZ5clMY80O1wSRqQjoi=s88-c-k-c0x00ffffff-no-rj" alt="logo" /></a></h1></strong>
   );
 }
